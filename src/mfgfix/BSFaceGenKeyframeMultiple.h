8d12205eac1db6cd4f1dcc82477d15799dba8f97
Scrabx3 <kris.vlntn@gmail.com>
2024-11-18 13:30:24 +0100

    format

--- src/mfgfix/BSFaceGenKeyframeMultiple.h
+++ src/mfgfix/BSFaceGenKeyframeMultiple.h
@@ -4,7 +4,7 @@
 
 namespace MfgFix
 {
-	class BSFaceGenKeyframeMultiple: public BSFaceGenKeyframe
+	class BSFaceGenKeyframeMultiple : public BSFaceGenKeyframe
 	{
 	public:
 		enum class Type : std::int32_t
@@ -83,34 +83,34 @@ namespace MfgFix
 			SkinnyMorph = 0
 		};
 
-							~BSFaceGenKeyframeMultiple() override;																// 00
-		bool				Unk_01(RE::BSTArray<BSFaceGenKeyframe*>& a_arr) override;											// 01
-		bool				Interpolate(BSFaceGenKeyframe* a_keyframe1, BSFaceGenKeyframe* a_keyframe2, float a_k) override;	// 02
-		bool				Interpolate(BSFaceGenKeyframe* a_keyframe, float a_k, bool a_ignoreSmall, bool a_copyBig) override;	// 03
-		void				Reset(bool a_initToZero) override;																	// 04
-		BSFaceGenKeyframe*	Clone() override;																					// 05
-		void				Copy(BSFaceGenKeyframe* a_keyframe) override;														// 06
-		bool				NotEqual(BSFaceGenKeyframe* a_keyframe) override;													// 07
-		float				GetMaxValue() override;																				// 08
-		bool				IsZero() override;																					// 09
-		bool				TransitionUpdate(float a_timeDelta, BSFaceGenKeyframe* a_keyframe) override;						// 0A
-		bool				NotZero() override;																					// 0B
-		bool				IsKeyframeMultiple() override;																		// 0C { return true; };
-		bool				IsKeyframeExclusive() override;																		// 0D { return false; };
+		~BSFaceGenKeyframeMultiple() override;																																		// 00
+		bool Unk_01(RE::BSTArray<BSFaceGenKeyframe*>& a_arr) override;																						// 01
+		bool Interpolate(BSFaceGenKeyframe* a_keyframe1, BSFaceGenKeyframe* a_keyframe2, float a_k) override;			// 02
+		bool Interpolate(BSFaceGenKeyframe* a_keyframe, float a_k, bool a_ignoreSmall, bool a_copyBig) override;	// 03
+		void Reset(bool a_initToZero) override;																																		// 04
+		BSFaceGenKeyframe* Clone() override;																																			// 05
+		void Copy(BSFaceGenKeyframe* a_keyframe) override;																												// 06
+		bool NotEqual(BSFaceGenKeyframe* a_keyframe) override;																										// 07
+		float GetMaxValue() override;																																							// 08
+		bool IsZero() override;																																										// 09
+		bool TransitionUpdate(float a_timeDelta, BSFaceGenKeyframe* a_keyframe) override;													// 0A
+		bool NotZero() override;																																									// 0B
+		bool IsKeyframeMultiple() override;																																				// 0C { return true; };
+		bool IsKeyframeExclusive() override;																																			// 0D { return false; };
 
-							// Is value in range [0, 1].
-		virtual bool		IsValueValid(std::uint32_t a_idx);																	// 0E
-		virtual void		Allocate(std::uint32_t a_count, bool a_initToZero);													// 0F
+		// Is value in range [0, 1].
+		virtual bool IsValueValid(std::uint32_t a_idx);										// 0E
+		virtual void Allocate(std::uint32_t a_count, bool a_initToZero);	// 0F
 
-		static std::string	GetValueName(Type a_type, std::uint32_t a_idx);
+		static std::string GetValueName(Type a_type, std::uint32_t a_idx);
 
-		void				SetValue(std::uint32_t a_idx, float a_value);
-		void				Reset();
+		void SetValue(std::uint32_t a_idx, float a_value);
+		void Reset();
 
-		float*			values;		// 10
-		std::uint32_t	count;		// 18
-		bool			isUpdated;	// 1C
-		std::uint8_t	pad1D[3];	// 1D
+		float* values;					// 10
+		std::uint32_t count;		// 18
+		bool isUpdated;					// 1C
+		std::uint8_t pad1D[3];	// 1D
 	};
 
 	static_assert(sizeof(BSFaceGenKeyframeMultiple) == 0x20);
